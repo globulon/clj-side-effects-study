f4e0308a4b03b9d0ebc7a685a416025dbdc5d783
Marc-Daniel Ortega <globulon@gmail.com>
2012-10-28 16:17:59 +0100

    Got a seed for web app

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject side-effect-study "1.0.0-SNAPSHOT"
+	:description "side-effect-study"
+	:plugins [[lein-ring "0.7.5"]]
+	:dev-dependencies [[ring-server "0.2.4"]]
+	:dependencies 	[[org.clojure/clojure "1.4.0"]
+					 [org.clojure/algo.monads "0.1.3-SNAPSHOT"]
+					 [org.clojure/java.jdbc "0.2.3"]
+					 [ring "1.1.6"]])
---
+++ src/com/promindis/study/web/core.clj
@@ -0,0 +1,63 @@
+(ns com.promindis.study.web.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [hiccup.core :as hiccup]
+            [hiccup.form :as form]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :as reload]
+            [ring.middleware.session :as session]
+            [ring.middleware.params :as params]
+            [ring.util.response :as response]))
+
+(defn registration-page [req]
+  (hiccup/html
+    [:html
+      [:body
+        (form/form-to
+          [:post "/register"]
+          [:fieldset
+            [:legend "Fill your personal information:"]
+            [:div 
+              (form/label :first-name "First name:")
+              [:div.controls
+                (form/text-field :first-name "")]]
+            [:div 
+              (form/label :last-name "Last name:")
+              [:div.controls
+                (form/text-field :last-name "")]]
+            [:div 
+              (form/label :login "Last name:")
+              [:div.controls
+                (form/text-field :login "")]]
+                (form/submit-button "Submit")])]]))
+
+(defn registration-successful [req]
+  (let [first-name (get-in req [:params "first-name"])]
+    (hiccup/html 
+      [:div 
+        (str "Registration done " first-name)])))
+
+(defn handle-registration [req]
+  (let [
+  	first-name (get-in req [:params "first-name"])
+  	last-name (get-in req [:params "last-name"])
+  	login (get-in req [:login "user-name"])]
+        (response/redirect (str "/done?first-name=" first-name))))
+
+(defn app-routes [req]
+  (routing req
+           (GET "/" [] "Welcome to the main page")
+           (GET "/register" [] registration-page)
+           (POST "/register" [] handle-registration)
+           (GET "/done" [] registration-successful)
+           (route/not-found "Page not found")))
+
+;;Gijs was using (reload/wrap-reload [his_namespace]) => why ?
+(def main-app
+     (-> app-routes
+         params/wrap-params
+         (reload/wrap-reload ['com.promindis.study.web.core])))
+
+(defn -main
+  [& args]
+  (jetty/run-jetty #'main-app {:port 8888 :join? false}))
